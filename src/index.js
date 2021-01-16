176c66bec29b2edc8f647585637f9088609596ca
totherain155 <totherain155@gmail.com>
2021-01-16 10:25:37 +0900

    practice1

--- src/index.js
+++ src/index.js
@@ -1,47 +1,25 @@
-import { createStore } from "redux";
+
+// redux toDoList 
 
 const form = document.querySelector("form"),
     input = form.querySelector("input"),
     ul = document.querySelector("ul")
 
 
-//showToDo
-const ADD_TODO = "ADD_TODO"
-const DELETE_TODO = "DELETE_TODO"
-
-const reducer = (state = [], action) => {
-
-    switch (action.type) {
-        case ADD_TODO:
-            return [...state, { text: action.text, id: Date.now() }]
-        case DELETE_TODO:
-            return []
-        default:
-            return state
-    }
-}
-
-const store = createStore(reducer)
-
-
-store.subscribe(() => console.log(store.getState()))
-
-/*
 const showToDo = (text) => {
-
     const li = document.createElement("li")
     li.innerText = text
     ul.appendChild(li)
 }
-*/
-
 
 
 const handleSubmit = (event) => {
     event.preventDefault()
     const value = input.value
     input.value = '';
-    store.dispatch({ type: ADD_TODO, text: value })
+    showToDo(value)
+
 }
+
 
 form.addEventListener("submit", handleSubmit)
